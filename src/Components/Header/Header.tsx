ef9f6070995806d8dc81bd0e43a583e85788801f
Yhago luis maciel de matos <yhago.matos@seap.ma.br>
2024-09-18 11:08:34 -0300

    Feito so o Header

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,19 @@
+import { StatsCard } from "../StatsCard/StatsCard";
+import styles from "./styles.module.scss";
+export const Header: React.FC = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <div>
+          <h1>MyTodo</h1>
+          <span>Bem-vindo, Yhago Luis</span>
+        </div>
+        <div>
+          <StatsCard title="Total de Tarefas:" value={5} />
+          <StatsCard title="Tarefas Pendentes:" value={4} />
+          <StatsCard title="Tarefas Concluindas" value={1} />
+        </div>
+      </div>
+    </header>
+  );
+};
